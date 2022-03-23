f6c299681e8ba7d5a3af9f8ba2ba43deaff6d48b
Programmierpeet <peter4thurner@gmail.com>
2022-03-23 11:37:47 +0100

    Aufgabe 2.2

--- PÜ2/example_read_data.py
+++ PÜ2/example_read_data.py
@@ -2,18 +2,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# %% Öffnen der Datei und konvertieren zu numpy-Array
-file_name =  'input_data/power_data_1.txt'
-power_data_watts = open(file_name).read().split("\n")
-x = np.array(power_data_watts)
+# %% Öffnen der drei Dateien, konvertieren zu numpy-Array und plotten
+def open_convert_plot():
+    for i in range (1,4):
+        file_name =  ('input_data/power_data_{0}.txt'.format(i))
+        power_data_watts = open(file_name).read().split("\n")
+        x = np.array(power_data_watts)
+        plt.title("Line graph {0}".format(i))
+        plt.plot(x, color="red")
+        plt.show()
 
-# %% Erstellen des Plots
-plt.title("Line graph")
-plt.plot(x, color="red")
 
-plt.show()
+# %% Ausführen der Funktion
+open_convert_plot()
 
 
-# %%
-
-# %%
