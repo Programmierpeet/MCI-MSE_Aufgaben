5afb25e17f9b4072fb1c2cd3f27d0b37c5599d69
so1502 <so1502@mci4me.at>
2022-03-27 19:21:18 +0200

    Änderung main_comments

--- PÜ2/main_1_comments_only.py
+++ PÜ2/main_1_comments_only.py
@@ -1,4 +1,27 @@
-# UC 2.0
+# UC 2.0 Auswertung der Leistungsdaten
+## Kontrolle der zuvor erstellten Daten durch Diagnostiker/in
 
 #%% UC 2.1 Einlesen der Daten
+## Einlesen der CSV-Files 'ecg_data_subject_i.csv' (i=1-3)
+
+#%% UC 2.2 Vorverarbeiten der Daten
+## Anpassung der Daten vom CSV-File falls nötig bei zB Fehlmessungen.
+## Vorverarbeitung der Daten aus dem CSV-File zur Visualisierung
+
+#%% UC 2.3 Analysieren der Daten auf Abbruchkriterium
+## Überprüfung ob Abbruchkriterium erfüllt ist
+## Evtl. manueller Abbruch des Tests bei Abbruchkriterium
+
+#%% UC 2.4 Erstellen einer Zusammenfassung
+## Auswertung der eingelesenen Daten
+## Evtl. Einfluss von Diagnostiker/in auf spezifische Auswertung
+
+#%% UC 2.5 Visualisierung der Daten
+## Plotten der zuvor ausgewerteten und ausgewählten Daten
+
+#%% UC 2.6 Manuelle Eingabe eines Abbruchkriteriums
+## Eingabe des Abbruchkriteriums wie gewünscht vor Speicherung.
+
+#%% UC 2.7 Speichern der Daten
+##Speichern der vorverarbeiteten Daten und Plots
 
